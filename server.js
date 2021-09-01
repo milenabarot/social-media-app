33fb4f00111df7d672afaaf2e3a1107a89e5257e
milenabarot <milenabarot@gmail.com>
2021-09-01 12:47:48 +0100

    Created User Model
    - Body validation in user model using express validator

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 //Connect Database
 connectDB();
 
+// Init Middleware
+// this allows us to get the data in req.body
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API Running"));
 
 // Define Routes
