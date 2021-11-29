18f305b21895655f04dfc779a34559bea85b6097
milenabarot <milenabarot@gmail.com>
2021-11-29 16:00:28 +0000

    Posts model and Add Post

---
+++ models/Post.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users",
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    // so we know which like came from which user
+    // and also makes sure a user doesn't like something multiple times
+    // creates am array of the Users objects
+    // mongoose automatically pluralizes the document to create the collection
+    // so 'users' is created from the creation of a 'user' document the 1st time we add a doc to the database
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Post = mongoose.model("post", PostSchema);
