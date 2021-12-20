0e2f959f3ff79add48b4e650d0b72e8ab8d9fcba
milenabarot <milenabarot@gmail.com>
2021-12-20 10:38:41 +0000

    Get & delete post routes
    - Get all posts
    - Get a post by ID
    - Delete post by ID

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,7 +6,7 @@ const Profile = require("../../models/Profile");
 const User = require("../../models/User");
 const Post = require("../../models/Post");
 
-// @route   POST api/post
+// @route   POST api/posts
 // @desc    Create a post
 // @access  Private
 
@@ -22,6 +22,8 @@ router.post(
 
     try {
       // need to get the user itself
+      // we are logged in, so have the token, which gives us the user ID
+      // and puts it inside of the request user ID so we can use that
       const user = await User.findById(req.user.id).select("-password");
 
       const newPost = new Post({
@@ -39,5 +41,77 @@ router.post(
     }
   }
 );
+
+// @route   GET api/posts
+// @desc    Get all posts
+// @access  Private
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   GET api/posts/:id
+// @desc    Get post by ID
+// @access  Private
+
+router.get("/:id", auth, async (req, res) => {
+  try {
+    // req.params.id allows us to get the id from the URL
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    // check to see if its the correct formatted ID passed in
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   DELETE api/posts/:id
+// @desc    Delete a post
+// @access  Private
+
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // if the post doesn't exist
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    //Check user
+    //need to make sure the user that owns the post is able to delete their own post
+    // have to convert the post.user to a string as it is currently an object, as
+    // comparing it to req.user.id which is a string
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+
+    await post.remove();
+
+    res.json({ msg: "Post removed" });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
