2760565fd0fbacd761ccff27582d7ac4163711f8
milenabarot <milenabarot@gmail.com>
2022-01-09 16:18:43 +0000

    Front end Client set up

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
