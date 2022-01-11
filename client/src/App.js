9b80978882b34a045562bce9aa7c15d2526808d7
milenabarot <milenabarot@gmail.com>
2022-01-11 11:08:04 +0000

    Set up of React Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar />
-      <Landing />
-    </div>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
